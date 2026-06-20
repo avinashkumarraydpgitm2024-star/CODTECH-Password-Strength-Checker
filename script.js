255422b608dc13d38657ecf1cb2d5ca2e8eddcee
avinashkumarraydpgitm2024-star <avinashkumarraydpgitm2024@gmail.com>
2026-06-20 08:57:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,48 @@
-body{
-    font-family: Arial, sans-serif;
-    background:#f4f4f4;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    height:100vh;
-    margin:0;
+function checkPassword() {
+
+    let password = document.getElementById("password").value;
+    let result = document.getElementById("result");
+    let fill = document.getElementById("strength-fill");
+
+    let score = 0;
+
+    if(password.length >= 8) score++;
+    if(/[A-Z]/.test(password)) score++;
+    if(/[a-z]/.test(password)) score++;
+    if(/[0-9]/.test(password)) score++;
+    if(/[^A-Za-z0-9]/.test(password)) score++;
+
+    if(score <= 2){
+        result.innerHTML = "❌ Weak Password";
+        result.style.color = "red";
+        fill.style.width = "33%";
+        fill.style.background = "red";
+    }
+    else if(score <= 4){
+        result.innerHTML = "⚠️ Medium Password";
+        result.style.color = "orange";
+        fill.style.width = "66%";
+        fill.style.background = "orange";
+    }
+    else{
+        result.innerHTML = "✅ Strong Password";
+        result.style.color = "green";
+        fill.style.width = "100%";
+        fill.style.background = "green";
+    }
 }
 
-.container{
-    background:#fff;
-    padding:30px;
-    width:420px;
-    border-radius:10px;
-    box-shadow:0 0 15px rgba(0,0,0,0.2);
-    text-align:center;
-}
+function togglePassword(){
 
-h1{
-    margin-bottom:20px;
-}
+    let password = document.getElementById("password");
+    let btn = document.getElementById("toggleBtn");
 
-.input-group{
-    display:flex;
-    gap:10px;
-}
-
-input{
-    flex:1;
-    padding:10px;
-    font-size:16px;
-}
-
-#toggleBtn{
-    padding:10px;
-}
-
-button{
-    margin-top:10px;
-    padding:10px 20px;
-    cursor:pointer;
-    border:none;
-    border-radius:5px;
-    background:#007bff;
-    color:white;
-}
-
-button:hover{
-    background:#0056b3;
-}
-
-.strength-bar{
-    width:100%;
-    height:12px;
-    background:#ddd;
-    border-radius:10px;
-    margin-top:15px;
-    overflow:hidden;
-}
-
-#strength-fill{
-    height:100%;
-    width:0%;
-    transition:0.4s;
-}
-
-#result{
-    margin-top:15px;
-    font-size:18px;
-    font-weight:bold;
-}
-
-.requirements{
-    text-align:left;
-    margin-top:20px;
+    if(password.type === "password"){
+        password.type = "text";
+        btn.innerHTML = "Hide";
+    }
+    else{
+        password.type = "password";
+        btn.innerHTML = "Show";
+    }
 }
